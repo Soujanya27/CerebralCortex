9a11c4a438fd60576311f2a0e53369bab577945d
Soujanya27 <soujanya.chatterjee@gmail.com>
2017-07-21 14:33:47 -0500

    Feature Vector DS updated

--- cerebralcortex/data_processor/feature/feature_vector.py
+++ cerebralcortex/data_processor/feature/feature_vector.py
@@ -21,12 +21,13 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from typing import List
 
 from cerebralcortex.kernel.datatypes.datapoint import DataPoint
 from cerebralcortex.kernel.datatypes.datastream import DataStream
 
 
-def generate_cStress_feature_vector(ecg_features: DataStream):
+def generate_cStress_feature_vector(ecg_features: List[DataStream]):
     """
     :param ecg_features: DataStream
     :return feature vector DataStream
@@ -35,13 +36,13 @@ def generate_cStress_feature_vector(ecg_features: DataStream):
     final_feature_vector = []
     for i in range(len(ecg_features[0].data)):
         feature_vector = []
-        for j in range(len(ecg_features)):
-            if ecg_features[j].data[i].sample is None:
+        for ef in ecg_features:
+            if ef.data[i].sample is None:
                 continue
-            feature_vector.append(ecg_features[j].data[i].sample)
+            feature_vector.append(ef.data[i].sample)
 
-        final_feature_vector.append(DataPoint.from_tuple(start_time=ecg_features[j].data[i].start_time,
-                                                         end_time=ecg_features[j].data[i].end_time,
+        final_feature_vector.append(DataPoint.from_tuple(start_time=ef.data[i].start_time,
+                                                         end_time=ef.data[i].end_time,
                                                          sample=feature_vector))
 
     feature_vector_ds = DataStream.from_datastream(ecg_features)
